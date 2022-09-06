6e6af2b38fd732d56b051b81cf7c52cd77cb57aa
Leonardo Gonçalves <leonardo@lginfo.com.br>
2022-09-05 21:44:15 -0300

    classe telefone

---
+++ src/main/java/br/com/lginfo/loucademia/domain/aluno/Telefone.java
@@ -0,0 +1,41 @@
+package br.com.lginfo.loucademia.domain.aluno;
+
+public class Telefone {
+	private Integer dddCelular;
+	private Integer dddFixo;
+	private Integer numCelular;
+	private Integer numFixo;
+
+	public Integer getDddCelular() {
+		return dddCelular;
+	}
+
+	public void setDddCelular(Integer dddCelular) {
+		this.dddCelular = dddCelular;
+	}
+
+	public Integer getDddFixo() {
+		return dddFixo;
+	}
+
+	public void setDddFixo(Integer dddFixo) {
+		this.dddFixo = dddFixo;
+	}
+
+	public Integer getNumCelular() {
+		return numCelular;
+	}
+
+	public void setNumCelular(Integer numCelular) {
+		this.numCelular = numCelular;
+	}
+
+	public Integer getNumFixo() {
+		return numFixo;
+	}
+
+	public void setNumFixo(Integer numFixo) {
+		this.numFixo = numFixo;
+	}
+
+}
