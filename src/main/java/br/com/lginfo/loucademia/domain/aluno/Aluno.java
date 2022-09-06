8bcac06ee0b32c5d7bff997c7018b737f411d75b
Leonardo Gonçalves <leonardo@lginfo.com.br>
2022-09-05 21:51:42 -0300

    classe aluno

---
+++ src/main/java/br/com/lginfo/loucademia/domain/aluno/Aluno.java
@@ -0,0 +1,95 @@
+package br.com.lginfo.loucademia.domain.aluno;
+
+import java.time.LocalDate;
+
+public class Aluno {
+	public enum Sexo {
+		Masculino, Feminino;
+	}
+
+	public enum Situacao {
+		Ativo, Inativo, Pendente;
+	}
+
+	private String matricula;
+	private String nome;
+	private Sexo sexo;
+	private Integer rg;
+	private LocalDate dataNascimento;
+	private Situacao situacao;
+	private String email;
+	private Endereco endereco = new Endereco();
+	private Telefone telefone = new Telefone();
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Sexo getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(Sexo sexo) {
+		this.sexo = sexo;
+	}
+
+	public Integer getRg() {
+		return rg;
+	}
+
+	public void setRg(Integer rg) {
+		this.rg = rg;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Situacao getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(Situacao situacao) {
+		this.situacao = situacao;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public Telefone getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(Telefone telefone) {
+		this.telefone = telefone;
+	}
+}
